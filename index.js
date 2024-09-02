6052916b31376bef3c47ba172277535d8b6ea503
Olga <olganikolsky@inbox.ru>
2024-09-02 23:02:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+let greeting = 'Hello,World'
+console.log (greeting)
+
+let a = 5
+let b = 10 
+let sum = a + b 
+console.log (sum)
+
+let name = 'Alice'
+name = 'Bob'
+console.log (name)
+
+let str = 'Hello'
+let num = 42
+let bool = true
+console.log (typeof str)
+console.log (typeof num)
+console.log (typeof bool)
+
+let age = 25 
+let str2 = String(age)
+console.log (str2)
+
+let firstName = 'John'
+let LastName = "Doe"
+let FullName = firstName + LastName
+console. log (FullName)
+
+let strNumber = '100'
+let num2 = +strNumber
+console.log (num2)
+
+let score = 95
+let str3 = String(score)
+console.log (str3)
+
+let isLoggedIn = true
+let str4 = String(isLoggedIn)
+console.log (str4)
